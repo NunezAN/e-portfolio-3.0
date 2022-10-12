2fb4eaa29ac89b073700a6bc76cd383512b5b2bf
NunezAN <alex101nunez@gmail.com>
2022-10-12 12:01:39 -0500

    responsive changes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -38,7 +38,7 @@ export default function Contact({}: Props) {
         Contact me
       </h3>
       <div className="flex flex-col space-y-10">
-        <h4 className="text-2xl md:text-4xl font-semibold text-center">
+        <h4 className="text-2xl md:text-4xl mt-20 md:mt-0 font-semibold text-center">
           Currently open to new opportunities,{" "}
           <span className="underline decoration-[#7289da]/50">Let&apos;s Talk.</span>
         </h4>
