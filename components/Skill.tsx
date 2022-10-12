d461f56f8e776a22b5d002632e14c5db99d28833
NunezAN <alex101nunez@gmail.com>
2022-10-12 18:42:59 -0500

    changed the width of mobile skill img

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -19,7 +19,7 @@ export default function Skill({ url, name }: Props) {
     >
       <img
         src={url}
-        className="w-16 object-fill md:w-28  xl:w-32 group-hover:scale-90 group-hover:brightness-90 transition duration-300 ease-in-out"
+        className="w-14 object-fill md:w-28  xl:w-32 group-hover:scale-90 group-hover:brightness-90 transition duration-300 ease-in-out"
         // className="w-16 h-12 object-fill md:w-28 md:h-24 xl:w-32 xl:h-28 group-hover:scale-90 group-hover:brightness-90 transition duration-300 ease-in-out"
       />
       <span className="opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out text-center mt-2 text:l xl:text-xl">
