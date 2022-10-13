21c4fc12a6623f46afcd3058e076a488e2c7fd01
NunezAN <alex101nunez@gmail.com>
2022-10-13 10:07:32 -0500

    changed hover effect of header icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { DocumentIcon } from "@heroicons/react/24/solid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 type Props = {};
 
 export default function Header({}: Props) {
-  // console.log(socials);
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
@@ -15,36 +17,37 @@ export default function Header({}: Props) {
         transition={{ duration: 1.5 }}
         className="flex flex-row items-center"
       >
-        <SocialIcon
-          url="https://www.linkedin.com/in/alexis-nunez-7b468624a/"
-          fgColor="gray"
-          bgColor="transparent"
+        <a
+          href="https://www.linkedin.com/in/alexis-nunez-7b468624a/"
           target="_blank"
-        />
-        <SocialIcon
-          url="https://github.com/NunezAN"
-          fgColor="gray"
-          bgColor="transparent"
-          target="_blank"
-        />
+        >
+          <FontAwesomeIcon
+            icon={faLinkedinIn}
+            className="text-[#808080] h-6 w-6 m-4 hover:text-white"
+          />
+        </a>
+        <a href="https://github.com/NunezAN" target="_blank">
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="text-[#808080] h-6 w-6 m-4 hover:text-white"
+          />
+        </a>
         <a href="resume.pdf" target="_blank">
-          <DocumentIcon className="text-[#808080] h-6 w-6 mx-2" />
+          <DocumentIcon className="text-[#808080] h-6 w-6 m-4 hover:text-white" />
         </a>
       </motion.div>
-      <Link href="#contact">
+      <Link href="#contact" className="hover:text-white">
         <motion.div
           initial={{ x: 500, opacity: 0, scale: 0.5 }}
           animate={{ x: 0, opacity: 1, scale: 1 }}
           transition={{ duration: 1.5 }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer"
+          className="flex flex-row items-center text-gray-300 cursor-pointer group"
         >
-          <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            fgColor="gray"
-            bgColor="transparent"
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            className="text-[#808080] h-6 w-6 m-4 group-hover:text-white"
           />
-          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400 group-hover:text-white">
             Let&apos;s have a chat
           </p>
         </motion.div>
